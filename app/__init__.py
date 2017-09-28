23a4ffcce6c85c4e9dab96bb1fb34b04863ddcbb
originaltebas <originaltebas@gmail.com>
2017-09-28 20:15:58 +0200

    creacion de Home, extras & auth: forms y views

--- app/__init__.py
+++ app/__init__.py
@@ -3,17 +3,15 @@
 #third-party imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-# after existing third-party imports
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 # local imports
 from config import app_config
 
 # db variable initialization
 db = SQLAlchemy()
-
 # after the db variable initialization
 login_manager = LoginManager()
 
@@ -21,6 +19,8 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    
+    Bootstrap(app)
     db.init_app(app)
 
     login_manager.init_app(app)
@@ -28,7 +28,16 @@ def create_app(config_name):
     login_manager.login_view = "auth.login"
 
     migrate = Migrate(app, db)
-
+    
     from app import models
+
+    from .extras import extras as extras_blueprint
+    app.register_blueprint(extras_blueprint, url_prefix='/extras')
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
     
     return app
--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,34 +1,7 @@
-#app/__init__.py
+# app/auth/__init__.py
 
-#third-party imports
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Blueprint
 
-# after existing third-party imports
-from flask_login import LoginManager
-from flask_migrate import Migrate
+auth = Blueprint('auth', __name__)
 
-# local imports
-from config import app_config
-
-# db variable initialization
-db = SQLAlchemy()
-
-# after the db variable initialization
-login_manager = LoginManager()
-
-def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
-    db.init_app(app)
-
-    login_manager.init_app(app)
-    login_manager.login_message = "Debes estar logado para acceder a esta pagina"
-    login_manager.login_view = "auth.login"
-
-    migrate = Migrate(app, db)
-
-    from app import models
-    
-    return app
+from . import views
