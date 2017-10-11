0867f872b38ff75297356b80b66282c4ffb60914
originaltebas <orginaltebas@gmail.com>
2017-10-11 22:15:27 +0200

    Agregados gestion grupos caseros y roles

--- app/extras/forms.py
+++ app/extras/forms.py
@@ -10,6 +10,14 @@ class FormGrupoCasero(FlaskForm):
     Formulario para agregar o editar un grupo casero
     """
     nombre_grupo = StringField(u'Nombre del Grupo', validators=[DataRequired()])
-    descripcion_grupo = StringField(u'Descripción del Grupo', validators=[DataRequired()])
+    descripcion_grupo = StringField(u'Día y Hora de reunión', validators=[DataRequired()])
     direccion_grupo = StringField(u'Dirección del Grupo', validators=[DataRequired()])
+    submit = SubmitField(u'Guardar')
+
+class FormRol(FlaskForm):
+    """
+    Formulario para agregar y editar roles (pastor, anciano, diacono, líder GC, )
+    """
+    nombre = StringField(u'Nombre', validators=[DataRequired()])
+    descripcion = StringField(u'Descripción', validators=[DataRequired()])
     submit = SubmitField(u'Guardar')
--- app/extras/views.py
+++ app/extras/views.py
@@ -5,9 +5,10 @@ from flask import abort, flash, redirect, render_template, url_for
 from flask_login import current_user, login_required
 
 from . import extras
-from forms import FormGrupoCasero
 from .. import db
-from ..models import GrupoCasero
+
+from forms import FormGrupoCasero, FormRol
+from ..models import GrupoCasero, Rol
 
 def check_admin():
     """
@@ -28,7 +29,7 @@ def listar_gruposcaseros():
 
     gruposcaseros = GrupoCasero.query.all()
 
-    return render_template('extras/gruposcaseros/gruposcaseros.html', departments=gruposcaseros, title="Grupos Caseros")
+    return render_template('extras/gruposcaseros/gruposcaseros.html', gruposcaseros=gruposcaseros, title="Grupos Caseros")
 
 @extras.route('/gruposcaseros/add', methods=['GET', 'POST'])
 @login_required
@@ -42,7 +43,7 @@ def add_grupocasero():
 
     form = FormGrupoCasero()
     if form.validate_on_submit():
-        gruposcasero = GrupoCasero(name=form.name.data,description=form.description.data)
+        gruposcasero = GrupoCasero(nombre_grupo=form.nombre_grupo.data,descripcion_grupo=form.descripcion_grupo.data,direccion_grupo=form.direccion_grupo.data)
         try:
             # add department to the database
             db.session.add(gruposcasero)
@@ -56,7 +57,7 @@ def add_grupocasero():
         return redirect(url_for('extras.listar_gruposcaseros'))
 
     # load department template
-    return render_template('extras/gruposcaseros/gruposcasero.html', action="Add", add_grupocasero=add_grupocasero, form=form, title="Agregar Grupo Casero")
+    return render_template('extras/gruposcaseros/grupocasero.html', action="Add", add_grupocasero=add_grupocasero, form=form, title="Agregar Grupo Casero")
 
 @extras.route('/gruposcaseros/edit/<int:id>', methods=['GET', 'POST'])
 @login_required
@@ -72,7 +73,7 @@ def edit_grupocasero(id):
     form = FormGrupoCasero(obj=grupocasero)
     if form.validate_on_submit():
         grupocasero.nombre_grupo = form.nombre_grupo.data
-        grupocasero.descricion_grupo = form.descripcion_grupo.data
+        grupocasero.descripcion_grupo = form.descripcion_grupo.data
         grupocasero.direccion_grupo = form.direccion_grupo.data
         db.session.commit()
         flash('Has modificado el grupo casero.')
@@ -80,15 +81,15 @@ def edit_grupocasero(id):
         # redirect to the departments page
         return redirect(url_for('extras.listar_gruposcaseros'))
 
-    form.descripcion_grupo = grupocasero.descricion_grupo
+    form.descripcion_grupo = grupocasero.descripcion_grupo
     form.nombre_grupo = grupocasero.nombre_grupo
-    return render_template('extras/gruposcaseros/gruposcasero.html', action="Edit",
+    return render_template('extras/gruposcaseros/grupocasero.html', action="Edit",
                            add_grupocasero = add_grupocasero, form=form,
                            grupocasero = grupocasero, title="Modificar Grupo Casero")
 
 @extras.route('/gruposcaseros/delete/<int:id>', methods=['GET', 'POST'])
 @login_required
-def delete_department(id):
+def delete_grupocasero(id):
     """
     Delete a department from the database
     """
@@ -103,3 +104,97 @@ def delete_department(id):
     return redirect(url_for('extras.listar_gruposcaseros'))
 
     return render_template(title="Borrar Grupos Caseros")
+
+
+"""
+AQUI FINALIZA CODIGO GRUPOS CASEROS
+-------------------------------------------------------------------------------------
+"""
+
+"""
+-------------------------------------------------------------------------------------
+AQUI COMIENZA CODIGO ROLES
+"""
+
+@extras.route('/roles')
+@login_required
+def listar_roles():
+    check_admin()
+    """
+    Listar todos los roles
+    """
+    roles = Rol.query.all()
+    return render_template('extras/roles/roles.html', roles=roles, title='Roles o Responsabilidades')
+
+@extras.route('/roles/add', methods=['GET', 'POST'])
+@login_required
+def add_rol():
+    """
+    Agregar un rol o responsabilidad a la base de datos
+    """
+    check_admin()
+
+    add_rol = True
+
+    form = FormRol()
+    if form.validate_on_submit():
+        rol = Rol(nombre=form.nombre.data, descripcion=form.descripcion.data)
+
+        try:
+            # add role to the database
+            db.session.add(rol)
+            db.session.commit()
+            flash('Has agregado un rol a la base de datos.')
+        except:
+            # in case role name already exists
+            flash('Error: el rol ya existe.')
+
+        # redirect to the roles page
+        return redirect(url_for('extras.listar_roles'))
+
+    # load role template
+    return render_template('extras/roles/rol.html', add_rol=add_rol, form=form, title='Agregar Rol o Responsabilidad')
+
+@extras.route('/roles/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_rol(id):
+    """
+    Modificar un Rol
+    """
+    check_admin()
+
+    add_rol = False
+
+    rol = Rol.query.get_or_404(id)
+    form = FormRol(obj=rol)
+    if form.validate_on_submit():
+        rol.nombre = form.nombre.data
+        rol.descripcion = form.descripcion.data
+        db.session.add(rol)
+        db.session.commit()
+        flash('Has modificado el rol en la base de datos.')
+
+        # redirect to the roles page
+        return redirect(url_for('extras.listar_roles'))
+
+    form.descripcion.data = rol.descripcion
+    form.nombre.data = rol.nombre
+    return render_template('admin/roles/rol.html', add_rol=add_rol, form=form, title="Modificar Rol o Responsabilidad")
+
+@extras.route('/roles/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete_rol(id):
+    """
+    Borrar un rol de la base de datos
+    """
+    check_admin()
+
+    rol = Rol.query.get_or_404(id)
+    db.session.delete(rol)
+    db.session.commit()
+    flash('Has borrado el rol de la base de datos.')
+
+    # redirect to the roles page
+    return redirect(url_for('extras.listar_roles'))
+
+    return render_template(title="Borrar Rol o Responsabilidad")
