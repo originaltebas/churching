40a99e6b7e0dff85f60fc8dc44ad8022ce98b3f8
originaltebas <orginaltebas@gmail.com>
2018-01-24 18:17:57 +0100

    rehacer la base de datos

--- migrations/versions/e7a284fc222a_.py
+++ migrations/versions/e7a284fc222a_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 396698708af2
+Revision ID: e7a284fc222a
 Revises: 
-Create Date: 2017-10-27 20:46:29.778299
+Create Date: 2018-01-23 14:08:15.686832
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '396698708af2'
+revision = 'e7a284fc222a'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -22,12 +22,6 @@ def upgrade():
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('nombre', sa.String(length=60), nullable=True),
     sa.Column('descripcion', sa.String(length=200), nullable=True),
-    sa.PrimaryKeyConstraint('id')
-    )
-    op.create_table('familias',
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('apellidos_familia', sa.String(length=60), nullable=True),
-    sa.Column('comentarios', sa.String(length=200), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('gruposcaseros',
@@ -44,6 +38,12 @@ def upgrade():
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('roles',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('nombre', sa.String(length=60), nullable=True),
+    sa.Column('descripcion', sa.String(length=200), nullable=True),
+    sa.PrimaryKeyConstraint('id')
+    )
+    op.create_table('tiposfamilias',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('nombre', sa.String(length=60), nullable=True),
     sa.Column('descripcion', sa.String(length=200), nullable=True),
@@ -67,11 +67,20 @@ def upgrade():
     )
     op.create_index(op.f('ix_usuarios_email'), 'usuarios', ['email'], unique=True)
     op.create_index(op.f('ix_usuarios_username'), 'usuarios', ['username'], unique=True)
+    op.create_table('familias',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('apellidos_familia', sa.String(length=60), nullable=True),
+    sa.Column('comentarios', sa.String(length=200), nullable=True),
+    sa.Column('id_tipofamilia', sa.Integer(), nullable=True),
+    sa.ForeignKeyConstraint(['id_tipofamilia'], ['tiposfamilias.id'], ),
+    sa.PrimaryKeyConstraint('id')
+    )
     op.create_table('miembros',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('id_familia', sa.Integer(), nullable=False),
     sa.Column('nombres', sa.String(length=100), nullable=True),
     sa.Column('apellidos', sa.String(length=100), nullable=True),
+    sa.Column('dni_doc', sa.String(length=20), nullable=True),
     sa.Column('id_parentezco', sa.Integer(), nullable=False),
     sa.Column('email', sa.String(length=60), nullable=True),
     sa.Column('id_estado_civil', sa.Integer(), nullable=False),
@@ -79,11 +88,12 @@ def upgrade():
     sa.Column('telefono_1', sa.String(length=15), nullable=True),
     sa.Column('telefono_2', sa.String(length=15), nullable=True),
     sa.Column('fecha_nac', sa.DateTime(), nullable=True),
+    sa.Column('fecha_inicio_icecha', sa.DateTime(), nullable=True),
     sa.Column('fecha_miembro', sa.DateTime(), nullable=True),
     sa.Column('fecha_bautismo', sa.DateTime(), nullable=True),
+    sa.Column('lugar_bautismo', sa.String(length=50), nullable=True),
     sa.Column('id_tipo_miembro', sa.Integer(), nullable=False),
-    sa.Column('asiste_regular', sa.Boolean(), nullable=True),
-    sa.Column('id_grupo_casero', sa.Integer(), nullable=False),
+    sa.Column('id_grupo_casero', sa.Integer(), nullable=True),
     sa.Column('observaciones', sa.String(length=500), nullable=True),
     sa.ForeignKeyConstraint(['id_estado_civil'], ['estadosciviles.id'], ),
     sa.ForeignKeyConstraint(['id_familia'], ['familias.id'], ),
@@ -111,13 +121,14 @@ def downgrade():
     op.drop_index(op.f('ix_miembros_email'), table_name='miembros')
     op.drop_index(op.f('ix_miembros_apellidos'), table_name='miembros')
     op.drop_table('miembros')
+    op.drop_table('familias')
     op.drop_index(op.f('ix_usuarios_username'), table_name='usuarios')
     op.drop_index(op.f('ix_usuarios_email'), table_name='usuarios')
     op.drop_table('usuarios')
     op.drop_table('tiposmiembros')
+    op.drop_table('tiposfamilias')
     op.drop_table('roles')
     op.drop_table('parentezcos')
     op.drop_table('gruposcaseros')
-    op.drop_table('familias')
     op.drop_table('estadosciviles')
     # ### end Alembic commands ###
