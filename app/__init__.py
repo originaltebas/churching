be5972c256f567d6ac84f611465e8d577b53c619
originaltebas <originaltebas@gmail.com>
2018-07-18 19:32:14 +0200

    modificacion completa del modelo de datos v3

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
-#app/__init__.py
+# app/__init__.py
 
-#third-party imports
+# third-party imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -15,20 +15,21 @@ db = SQLAlchemy()
 # after the db variable initialization
 login_manager = LoginManager()
 
+
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-    
+
     Bootstrap(app)
     db.init_app(app)
 
     login_manager.init_app(app)
-    login_manager.login_message = "Debes estar logado para acceder a esta pagina"
+    login_manager.login_message = "Debes estar logado para ver esta pagina"
     login_manager.login_view = "auth.login"
 
     migrate = Migrate(app, db)
-    
+
     from app import models
 
     from .extras import extras as extras_blueprint
@@ -39,5 +40,5 @@ def create_app(config_name):
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
-    
+
     return app
--- app/models.py
+++ app/models.py
@@ -5,179 +5,353 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db, login_manager
 
-
-miembros_roles = db.Table('miembros_roles',
-    db.Column('id_miembro', db.Integer, db.ForeignKey('miembros.id'),nullable=False),
-    db.Column('id_rol', db.Integer, db.ForeignKey('roles.id'),nullable=False)
+miembros_parientes = db.Table(
+                              'asoc_miembros_parientes',
+                              db.metadata,
+                              db.Column('asoc_id_miembro',
+                                        db.Integer,
+                                        db.ForeignKey('miembros.id')),
+                              db.Column('asoc_id_pariente',
+                                        db.Integer,
+                                        db.ForeignKey('parientes.id'))
 )
-
-miembros_parientes = db.Table('miembros_parientes',
-    db.Column('id_miembro_origen', db.Integer, db.ForeignKey('miembros.id'),nullable=False),
-    db.Column('id_miembro_destino', db.Integer, db.ForeignKey('miembros.id'),nullable=False),
-    db.Column('id_parentezco', db.Integer, db.ForeignKey('parentezcos.id'),nullable=False)
-)
-
 
 
 class Miembro(db.Model):
-    """
-    Crear una tabla de miembros
-    """
+    "TABLA BASE DEL MODELO - MIEMBROS DE LA IGLESIA"
 
-    # Ensures table will be named in plural and not in singular
-    # as is the name of the model
+    # NOMBRE DE TABLA EN MYSQL
     __tablename__ = 'miembros'
 
+    # CLAVE PRIMARIA
     id = db.Column(db.Integer, primary_key=True)
-    id_familia = db.Column(db.Integer, db.ForeignKey('familias.id'),nullable=False)
-    nombres = db.Column(db.String(100), index=True)
-    apellidos = db.Column(db.String(100), index=True)
+
+    # CAMPOS DESCRIPTIVOS
+    nombres = db.Column(db.String(100), index=True, nullable=False)
+    apellidos = db.Column(db.String(100), index=True, nullable=False)
     dni_doc = db.Column(db.String(20))
     email = db.Column(db.String(60), index=True, unique=True)
-    id_estado_civil = db.Column(db.Integer, db.ForeignKey('estadosciviles.id'),nullable=False)
-    direccion = db.Column(db.String(200))
-    telefono_1 = db.Column(db.String(15))
-    telefono_2 = db.Column(db.String(15))
-    fecha_nac = db.Column(db.DateTime)
+    fecha_nac = db.Column(db.DateTime, nullable=False)
     fecha_inicio_icecha = db.Column(db.DateTime)
     fecha_miembro = db.Column(db.DateTime)
     fecha_bautismo = db.Column(db.DateTime)
     lugar_bautismo = db.Column(db.String(50))
-    id_tipo_miembro = db.Column(db.Integer, db.ForeignKey('tiposmiembros.id'),nullable=False)
-    id_grupo_casero = db.Column(db.Integer, db.ForeignKey('gruposcaseros.id'))
     observaciones = db.Column(db.String(500))
 
-    pariente_origen = db.relationship(
-        'Miembro', secondary=miembros_parientes,
-        primaryjoin=(miembros_parientes.c.id_miembro_destino == id),
-        secondaryjoin=(miembros_parientes.c.id_miembro_origen == id),
-        backref=db.backref('pariente_destino', lazy='dynamic'), lazy='dynamic')
+    # RELACIONES 1:1 [FOREINGKEYS]
+    # Direccion
+    id_direccion = db.Column(db.Integer, db.ForeignKey('direcciones.id'),
+                             nullable=False)
+    direccion = db.relationship("Direccion", back_populates="miembro")
+
+    # Estado Civil
+    id_estadoscivil = db.Column(db.Integer, db.ForeignKey('estadosciviles.id'),
+                                nullable=False)
+    estadocivil = db.relationship("EstadoCivil", back_populates="miembro")
+
+    # Tipo de Miembro
+    id_tipomiembro = db.Column(db.Integer, db.ForeignKey('tiposmiembros.id'),
+                               nullable=False)
+    tipomiembro = db.relationship("TipoMiembro", back_populates="miembro")
+
+    # Grupo Casero
+    id_grupocasero = db.Column(db.Integer, db.ForeignKey('gruposcaseros.id'),
+                               nullable=False)
+    grupocasero = db.relationship("GrupoCasero", back_populates="miembro")
+
+    # RELACIONES 0:N // 1:N // N:N
+    # FAMILIAS  1:N
+    familias = db.relationship("Familia", back_populates="miembro")
+
+    # TELEFONOS 1:N
+    telefonos = db.relationship("Telefono", back_populates="miembro")
+
+    # ROLES 1:N
+    roles = db.relationship("Rol", back_populates="miembro")
+
+    # ASISTENCIAS 1:N
+    asistencias = db.relationship("Asistencia", back_populates="miembro")
+
+    # SEGUIMIENTOS 1:N
+    seguimientos = db.relationship("Seguimiento", back_populates="miembro")
+
+    # PARIENTES N:N
+    parientes = db.relationship("Pariente", secondary=miembros_parientes,
+                                back_populates="miembros")
 
     def __repr__(self):
-        return '<Miembro: %s ' ' %s>' %(self.nombres,self.apellidos)
+        return '<Miembro: %s ' ' %s>' % (self.nombres, self.apellidos)
+
+
+class Direccion(db.Model):
+    "TABLA GENERAL DE DIRECCIONES DE MIEMBROS / FAMILIAS / GRUPOS CASEROS"
+
+    # NOMBRE DE TABLA EN MYSQL
+    __tablename__ = 'direcciones'
+
+    # CLAVE PRIMARIA
+    id = db.Column(db.Integer, primary_key=True)
+
+    # CAMPOS DESCRIPTIVOS
+    tipo_via = db.Column(db.String(20), nullable=False)
+    nombre_via = db.Column(db.String(100), nullable=False)
+    nro_via = db.Column(db.String(10), nullable=False)
+    portalescalotros_via = db.Column(db.String(20))
+    cp_via = db.Column(db.String(10), nullable=False)
+    ciudad_via = db.Column(db.String(20), nullable=False)
+    provincia_via = db.Column(db.String(20), nullable=False)
+    pais_via = db.Column(db.String(20), nullable=False)
+
+    # RELACIONES 1:1 [FOREINGKEYS]
+    miembro = db.relationship('Miembro', uselist=False,
+                              back_populates='direccion')
+    familia = db.relationship('Miembro', uselist=False,
+                              back_populates='familia')
+    grupocasero = db.relationship('Miembro', uselist=False,
+                                  back_populates='grupocasero')
+
+    def __repr__(self):
+        return '<Direccion: %s ' ' %s ' ' %s>' % (self.tipo_via,
+                                                  self.nombre_via,
+                                                  self.nro_via)
+
+
+class EstadoCivil(db.Model):
+    "TABLA TIPO DE ESTADO CIVIL"
+
+    # NOMBRE DE TABLA EN MYSQL
+    __tablename__ = 'estadosciviles'
+
+    # CLAVE PRIMARIA
+    id = db.Column(db.Integer, primary_key=True)
+
+    # CAMPOS DESCRIPTIVOS
+    nombre_estado = db.Column(db.String(60), nullable=False)
+    descripcion_estado = db.Column(db.String(200))
+
+    # RELACIONES 1:1 [FOREINGKEYS]
+    miembro = db.relationship('Miembro', uselist=False,
+                              back_populates='estadocivil')
+
+    def __repr__(self):
+        return '<Estado Civil: {}>'.format(self.nombre_estado)
+
+
+class TipoMiembro(db.Model):
+    "TABLA TIPO DE ESTADO CIVIL"
+
+    # NOMBRE DE TABLA EN MYSQL
+    __tablename__ = 'tiposmiembros'
+
+    # CLAVE PRIMARIA
+    id = db.Column(db.Integer, primary_key=True)
+
+    # CAMPOS DESCRIPTIVOS
+    nombre_tipomiembro = db.Column(db.String(60), nullable=False)
+    descripcion_tipomiembro = db.Column(db.String(200))
+
+    # RELACIONES 1:1 [FOREINGKEYS]
+    miembro = db.relationship('Miembro', uselist=False,
+                              back_populates='tipomiembro')
+
+    def __repr__(self):
+        return '<Tipo de Miembro: {}>'.format(self.nombre)
+
 
 class GrupoCasero(db.Model):
-    """
-    Crear una tabla de grupos caseros
-    """
+    "TABLA DE GRUPOS CASEROS / GRUPOS CELULA / GRUPOS PEQUEÑOS / SMALL GROUPS"
 
+    # NOMBRE DE TABLA EN MYSQL
     __tablename__ = 'gruposcaseros'
 
+    # CLAVE PRIMARIA
     id = db.Column(db.Integer, primary_key=True)
-    nombre_grupo = db.Column(db.String(60),nullable=False)
-    descripcion_grupo = db.Column(db.String(200),nullable=False)
-    direccion_grupo = db.Column(db.String(200),nullable=False)
-    miembros = db.relationship('Miembro', backref='grupocasero', lazy='dynamic')
+
+    # CAMPOS DESCRIPTIVOS
+    nombre_grupo = db.Column(db.String(60), nullable=False)
+    descripcion_grupo = db.Column(db.String(200))
+
+    # RELACIONES 1:1 [FOREINGKEYS]
+    id_direccion = db.Column(db.Integer, db.ForeignKey('direcciones.id'),
+                             nullable=False)
+    miembro = db.relationship('Miembro', uselist=False,
+                              back_populates='grupocasero')
 
     def __repr__(self):
         return '<Grupo Casero: {}>'.format(self.nombre_grupo)
 
 
-class Rol(db.Model):
-    """
-    Crea una tabla de funciones y roles
-    """
-
-    __tablename__ = 'roles'
-
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(60))
-    descripcion = db.Column(db.String(200))
-    miembros = db.relationship('Miembro', secondary=miembros_roles, backref=db.backref('roles', lazy='dynamic'))
-
-
-    def __repr__(self):
-        return '<Rol: {}>'.format(self.nombre)
-
-class Parentezco(db.Model):
-    """
-    Crea una tabla de parentezcos (padre, madre, hermano, nieto, abuelo)
-    se carga todas las relaciones que tenga cada persona con otros de la iglesia
-    """
-
-    __tablename__ = 'parentezcos'
-
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(60))
-    descripcion = db.Column(db.String(200))
-
-    def __repr__(self):
-        return '<Parentezco: {}>'.format(self.nombre)
-
-class EstadoCivil(db.Model):
-    """
-    Crea una tabla de estados civiles
-    """
-
-    __tablename__ = 'estadosciviles'
-
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(60))
-    descripcion = db.Column(db.String(200))
-    miembros = db.relationship('Miembro', backref='estado',lazy='dynamic')
-
-
-    def __repr__(self):
-        return '<Estado Civil: {}>'.format(self.nombre)
-
-class TipoMiembro(db.Model):
-    """
-    Crea una tabla de tipos de miembros ( miembro, asistente regular, no viene, etc)
-    """
-
-    __tablename__ = 'tiposmiembros'
-
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(60))
-    descripcion = db.Column(db.String(200))
-    miembros = db.relationship('Miembro', backref='tipomiembro',lazy='dynamic')
-
-
-    def __repr__(self):
-        return '<Tipo de Miembro: {}>'.format(self.nombre)
-
 class Familia(db.Model):
-    """
-    Crea una tabla familias
-    """
+    "TABLA DE FAMILIAS"
 
+    # NOMBRE DE TABLA EN MYSQL
     __tablename__ = 'familias'
 
+    # CLAVE PRIMARIA
     id = db.Column(db.Integer, primary_key=True)
-    apellidos_familia = db.Column(db.String(60))
-    comentarios = db.Column(db.String(200))
-    id_tipofamilia = db.Column(db.Integer, db.ForeignKey('tiposfamilias.id'))
-    miembros = db.relationship('Miembro', backref='familia',lazy='dynamic')
+
+    # CAMPOS DESCRIPTIVOS
+    apellidos_familia = db.Column(db.String(60), nullable=False)
+    descripcion_familia = db.Column(db.String(200))
+
+    # RELACIONES 1:1 [FOREINGKEYS]
+    id_direccion = db.Column(db.Integer, db.ForeignKey('direcciones.id'),
+                             nullable=False)
+
+    # RELACIONES 0:N // 1:N // N:N
+    # MIEMBROS  N:1
+    id_miembro = db.Column(db.Integer, db.ForeignKey('miembros.id'))
+    miembro = db.relationship('Miembro', back_populates='familias')
 
     def __repr__(self):
         return '<Familia: {}>'.format(self.apellidos_familia)
 
-class TipoFamilia(db.Model):
-    """
-    Crea una tabla tipo familia- nucleares (padre,madre,hijos), monoparentales, extendidas(nuclear+parientes), otras
-    """
-    __tablename__ = 'tiposfamilias'
 
+class Telefono(db.Model):
+    "TABLA DE TELEFONOS FIJOS Y MOVILES"
+
+    # NOMBRE DE TABLA EN MYSQL
+    __tablename__ = 'telefonos'
+
+    # CLAVE PRIMARIA
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(60))
-    descripcion = db.Column(db.String(200))
-    familias = db.relationship('Familia', backref='tipofamilia',lazy='dynamic')
+
+    # CAMPOS DESCRIPTIVOS
+    numero_tel = db.Column(db.String(20), nullable=False)
+    tipo_tel = db.Column(db.String(20), nullable=False)  # movil,casa,trabajo
+
+    # RELACIONES 0:N // 1:N // N:N
+    # MIEMBROS  N:1
+    id_miembro = db.Column(db.Integer, db.ForeignKey('miembros.id'))
+    miembro = db.relationship('Miembro', back_populates='telefonos')
 
     def __repr__(self):
-        return '<Tipo de Familia: {}>'.format(self.nombre)
+        return '<Telefono: {}>'.format(self.numero)
+
+
+class Rol(db.Model):
+    "TABLA DE TELEFONOS FIJOS Y MOVILES"
+
+    # NOMBRE DE TABLA EN MYSQL
+    __tablename__ = 'roles'
+
+    # CLAVE PRIMARIA
+    id = db.Column(db.Integer, primary_key=True)
+
+    # CAMPOS DESCRIPTIVOS
+    nombre_rol = db.Column(db.String(60), nullable=False)
+    descripcion_rol = db.Column(db.String(200))
+
+    # RELACIONES 0:N // 1:N // N:N
+    # MIEMBROS  N:1
+    id_miembro = db.Column(db.Integer, db.ForeignKey('miembros.id'))
+    miembro = db.relationship('Miembro', back_populates='roles')
+
+    def __repr__(self):
+        return '<Rol: {}>'.format(self.nombre)
+
+
+class Pariente(db.Model):
+    "TABLA ids de miembros para relacionarlos con ellos misma."
+    "miembros parientes de miembros"
+
+    # NOMBRE DE TABLA EN MYSQL
+    __tablename__ = 'parientes'
+
+    # CLAVE PRIMARIA
+    id = db.Column(db.Integer, primary_key=True)
+
+    # CAMPOS DESCRIPTIVOS
+    descripcion_pariente = db.Column(db.String(200))
+
+    # RELACIONES N:N
+    miembros = db.relationship("Miembro", secondary=miembros_parientes,
+                               back_populates="parientes")
+
+    # RELACIONES 1:1 [FOREINGKEYS]
+    # TipoParentezco
+    id_tipopariente = db.Column(db.Integer, db.ForeignKey('parentezcos.id'),
+                                nullable=False)
+    tipopariente = db.relationship("TipoParentezco", back_populates="pariente")
+
+
+class TipoParentezco(db.Model):
+    "TABLA DE TIPOS DE PARENTEZCO (padre, madre, hermano, nieto, abuelo)"
+
+    # NOMBRE DE TABLA EN MYSQL
+    __tablename__ = 'parentezcos'
+
+    # CLAVE PRIMARIA
+    id = db.Column(db.Integer, primary_key=True)
+
+    # CAMPOS DESCRIPTIVOS
+    nombre_parentezco = db.Column(db.String(60), nullable=False)
+    descripcion_parentezco = db.Column(db.String(200))
+
+    # RELACIONES 1:1 [FOREINGKEYS]
+    # Pariente
+    pariente = db.relationship('Pariente', uselist=False,
+                               back_populates='tipoparentezco')
+
+    def __repr__(self):
+        return '<Parentezco: {}>'.format(self.nombre_parentezco)
+
+
+class Asistencia(db.Model):
+    "TABLA DE ASISTENCIAS - PARA SABER QUIEN FALTA Y PODER CONTACTARLO"
+
+    # NOMBRE DE TABLA EN MYSQL
+    __tablename__ = 'asistencias'
+
+    # CLAVE PRIMARIA
+    id = db.Column(db.Integer, primary_key=True)
+
+    # CAMPOS DESCRIPTIVOS
+    fecha_culto = db.Column(db.DateTime, nullable=False)
+    asistio = db.Column(db.Boolean, nullable=False)
+
+    # MIEMBROS  N:1
+    id_miembro = db.Column(db.Integer, db.ForeignKey('miembros.id'))
+    miembro = db.relationship('Miembro', back_populates='asistencias')
+
+    def __repr__(self):
+        return '<Asistencia: %s>' '%s' % (self.fecha_culto,
+                                          self.asistio)
+
+
+class Seguimiento(db.Model):
+    "TABLA DE SEGUIMIENTO - COMENTARIOS SOBRE LLAMADAS "
+    "Y COSAS QUE PUEDE SER BUENO SABER"
+
+    # NOMBRE DE TABLA EN MYSQL
+    __tablename__ = 'seguimientos'
+
+    # CLAVE PRIMARIA
+    id = db.Column(db.Integer, primary_key=True)
+
+    # CAMPOS DESCRIPTIVOS
+    fecha_seg = db.Column(db.DateTime, nullable=False)
+    comentarios_seg = db.Column(db.Boolean, nullable=False)
+
+    # MIEMBROS  N:1
+    id_miembro = db.Column(db.Integer, db.ForeignKey('miembros.id'))
+    miembro = db.relationship('Miembro', back_populates='seguimientos')
+
+    def __repr__(self):
+        return '<Seguimiento: %s>' '%s' % (self.fecha_seg,
+                                           self.comentarios_seg)
 
 
 class Usuario(UserMixin, db.Model):
-    """
-    Crear una tabla de usuarios de la aplicacion
-    """
+    "tabla de usuarios de la aplicacion -- no tiene relacion con el modelo"
 
-    # Ensures table will be named in plural and not in singular
-    # as is the name of the model
+    # NOMBRE DE TABLA EN MYSQL
     __tablename__ = 'usuarios'
 
-
+    # CLAVE PRIMARIA
     id = db.Column(db.Integer, primary_key=True)
+
+    # CAMPOS DESCRIPTIVOS
     email = db.Column(db.String(60), index=True, unique=True)
     username = db.Column(db.String(60), index=True, unique=True)
     first_name = db.Column(db.String(60))
@@ -204,6 +378,7 @@ class Usuario(UserMixin, db.Model):
         Check if hashed password matches actual password
         """
         return check_password_hash(self.password_hash, password)
+
 
 # Set up user_loader
 @login_manager.user_loader
